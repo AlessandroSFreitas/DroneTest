bddd3e073ede966f5b18837b53efef2b084fe8af
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-10-19 17:32:46 -0300

    fixz z eixo

--- scripts/main.js
+++ scripts/main.js
@@ -20,9 +20,8 @@
 
       // telling the body about the direction for the animation
       if (alpha > 0) {
+        drone.style.width = (alpha * 0.5) + 'px';
         document.body.setAttribute('data-moving', 'right');
-        line.style.width = (alpha * 1.5) + 'px';
-        line.style.transform = 'translateZ(5px)';
       }
 
       // if (alpha < 0) {
@@ -37,13 +36,13 @@
       // rotating the balloon
       // balloon.style.transform = "rotateZ("+(1*(alpha + wip))+"deg)";
 
-      // tm = setTimeout(_=>{
-      //   // ending the wipplash effect
-      //   balloon.style.transform = "rotateZ("+(-1*(alpha - wip))+"deg)";
-      // }, 400);
+      tm = setTimeout(_=>{
+        // ending the wipplash effect
+        drone.style.transform = "rotateX("+(-1*(gamma - wip))+"deg)";
+      }, 400);
 
       // and now we store the gamma
-      // lastZ = alpha;
+      lastY = alpha;
     }
 
     // gatting farther or closer (Eixo X)
