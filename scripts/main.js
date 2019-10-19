11d37c1b1e39b4a833f405676c2d92b78e35257a
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-10-19 17:26:33 -0300

    fixz z eixo

--- scripts/main.js
+++ scripts/main.js
@@ -22,25 +22,25 @@
       if (alpha > 0) {
         document.body.setAttribute('data-moving', 'right');
         line.style.width = (alpha * 1.5) + 'px';
-        line.style.transform = 'translateX(5px)';
+        line.style.transform = 'translateZ(5px)';
       }
 
       if (alpha < 0) {
         document.body.setAttribute('data-moving', 'left');
         line.style.width = (-alpha * 1.1) + 'px';
-        line.style.transform = 'translateX(-5px)';
+        line.style.transform = 'translateZ(-5px)';
       }
 
       // adding wipplash effect
-      wip = (alpha - lastZ)/wipDivizor;
+      // wip = (alpha - lastZ)/wipDivizor;
 
       // rotating the balloon
-      balloon.style.transform = "rotateZ("+(1*(alpha + wip))+"deg)";
+      // balloon.style.transform = "rotateZ("+(1*(alpha + wip))+"deg)";
 
-      tm = setTimeout(_=>{
-        // ending the wipplash effect
-        balloon.style.transform = "rotateZ("+(-1*(alpha - wip))+"deg)";
-      }, 400);
+      // tm = setTimeout(_=>{
+      //   // ending the wipplash effect
+      //   balloon.style.transform = "rotateZ("+(-1*(alpha - wip))+"deg)";
+      // }, 400);
 
       // and now we store the gamma
       lastZ = alpha;
